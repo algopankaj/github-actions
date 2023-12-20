cc8c5c71ec26511d428f89d1e6e1f4cb2a7466d9
algopankaj <pankaj21905@gmail.com>
2023-12-20 13:49:18 +0530

    added

--- src/main/java/com/example/demo6/Demo6Application.java
+++ src/main/java/com/example/demo6/Demo6Application.java
@@ -20,10 +20,3 @@ public class Demo6Application {
 
 }
 
-//echo "# github-actions" >> README.md
-//git init
-//git add README.md
-//git commit -m "first commit"
-//git branch -M main
-//git remote add origin https://github.com/algopankaj/github-actions.git
-//git push -u origin main
